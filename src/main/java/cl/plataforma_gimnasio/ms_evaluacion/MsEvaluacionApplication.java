d3bd153037c1d9d15d6876656b018f029f4bbda0
ji-munoza <ji.munoza@duocuc.cl>
2026-05-17 22:58:56 -0400

    añadido discovery client

--- src/main/java/cl/plataforma_gimnasio/ms_evaluacion/MsEvaluacionApplication.java
+++ src/main/java/cl/plataforma_gimnasio/ms_evaluacion/MsEvaluacionApplication.java
@@ -2,7 +2,9 @@ package cl.plataforma_gimnasio.ms_evaluacion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MsEvaluacionApplication {
 
