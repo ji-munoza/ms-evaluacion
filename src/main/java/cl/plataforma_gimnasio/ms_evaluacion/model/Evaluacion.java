76dc3f60b551838a53adb292b963d5094a60c71f
ji-munoza <ji.munoza@duocuc.cl>
2026-05-18 00:10:21 -0400

    corregido nombre de tabla

--- src/main/java/cl/plataforma_gimnasio/ms_evaluacion/model/Evaluacion.java
+++ src/main/java/cl/plataforma_gimnasio/ms_evaluacion/model/Evaluacion.java
@@ -10,7 +10,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "evaluaciones_fisicas")
+@Table(name = "evaluaciones")
 public class Evaluacion {
 
     @Id
